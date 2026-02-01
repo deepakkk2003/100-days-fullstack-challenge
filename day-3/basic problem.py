920772f64c4b745922698cb50d3eb9dcebf4bab5
deepakkk2003 <deepakkothakonda666@gmail.com>
2026-02-01 21:06:49 +0530

    DAY-3

---
+++ day-3/basic problem.py
@@ -0,0 +1,21 @@
+n=int(input())
+for i in range(1,n+1):
+    print(i)
+    
+    
+
+N = int(input())
+total = 0
+
+for i in range(1, N + 1):
+    total = total + i
+
+print(total)
+
+
+N = int(input())
+
+if N % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
---
+++ day-3/objects.js
@@ -0,0 +1,66 @@
+// let user = {
+//   name: "KK.Deepak",
+//   age: 23,
+//   email: "deepak@gmail.com"
+// };
+
+// console.log(user);
+
+// //Accessing object data
+
+// console.log(user.name);
+// console.log(user["age"]);
+
+
+// let product={
+//     name:"Powder",
+//     price:200,
+//     inStock:true
+// }
+
+// console.log(product.name);
+// console.log(product.price);
+// console.log(product.inStock);
+
+// product.price=250;
+
+// let category
+// product.category="Liqiud"
+// delete product.inStock;
+// console.log(product);
+
+// function printUserDetails(user) {
+//   console.log(user.name);
+//   console.log(user.age);
+// }
+
+// printUserDetails(user)
+
+// function productDetails(product){
+//     console.log(product.name);
+//     console.log(product.price);
+    
+// }
+
+// productDetails(product)
+
+// for(let key in user){
+//     console.log(key,user[key]);
+// }
+
+let student={
+    name:"Deepak",
+    marks:56
+}
+
+function checkResult(student){
+    if(student.marks>35){
+        console.log("pass");
+    }
+    else {
+        console.log("Fail");
+    }
+}
+
+checkResult(student)
+
